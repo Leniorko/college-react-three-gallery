9e4aabea30c828b3f8a726a95db32fc5053d57c1
Ivan Egorov <www.Len244@gmail.com>
2021-11-22 16:24:31 +0300

    Moved to react fiber

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,39 +1,13 @@
-import { useEffect, useRef } from "react";
-import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import Box from "./Box";
 
 export default function Scene() {
-  const sceneContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-
-    const renderer = new THREE.WebGL1Renderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    camera.position.z = 5;
-    if (sceneContainer.current !== null) {
-      sceneContainer.current.appendChild(renderer.domElement);
-    }
-
-    function animate() {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-    }
-    animate();
-  }, []);
-
-  return <div ref={sceneContainer}></div>;
+  return (
+    <Canvas style={{ height: "100vh" }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
 }
