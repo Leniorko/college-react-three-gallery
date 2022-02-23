2eeddaf7d6f26db75fde3ad7a300960a39b97837
Ivan Egorov <www.Len244@gmail.com>
2022-02-23 16:51:01 +0300

    Code and error cleanup

---
+++ src/components/CameraControlsConfig.tsx
@@ -0,0 +1,49 @@
+import { PointerLockControls } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { Vector3 } from "three";
+
+/**
+ * Component to define custom camera ant controls
+ */
+// FIXME: Not removing event listener on unlock (code deleted and not worked before)
+// FIXME: During development on hot reload adds new event listeners. Shouldn't be a problem on prod.
+// FIXME: Jittering movement. Should be smooth. Probably onKeyDown should add velocity and stop it onKeyUp
+export default function CustomControls() {
+  const controls = useRef<any>(null);
+
+  const { camera } = useThree();
+
+  useEffect(()=>{
+    camera.position.set(0.5 , 1.8, -2)
+    camera.lookAt(1, 1.8, -2);    
+  }, [])
+
+  useEffect(()=>{
+    const onKeyDown = function (event: KeyboardEvent) {
+      switch (event.code) {
+        case "KeyW":
+          controls.current.moveForward(0.11);
+          break;
+        case "KeyA":
+          controls.current.moveRight(-0.11);
+          break;
+        case "KeyS":
+          controls.current.moveForward(-0.11);
+          break;
+        case "KeyD":
+          controls.current.moveRight(0.11);
+          break;
+      }
+    };
+    if (controls.current){
+      document.addEventListener("keydown", onKeyDown, false);
+    }
+    
+  }, [controls.current])
+  
+ 
+  return (
+    <PointerLockControls ref={controls}/>
+  );
+}
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,16 +1,9 @@
-import { Camera, Canvas, PerspectiveCameraProps, useLoader, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useRef, useState } from "react";
-import THREE, { Color, Vector3 } from "three";
-import {
-  Environment,
-  OrbitControls,
-  PerspectiveCamera,
-  PointerLockControls,
-  useGLTF,
-} from "@react-three/drei";
-import modelURL from "../threejsmodels/kindoflamp.gltf";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef } from "react";
+import THREE, { Color } from "three";
+import { Environment } from "@react-three/drei";
 import GalleryModel from "./GalleryModel";
-import { GLTFResult } from "../types/common.types";
+import CustomControls from "./CameraControlsConfig";
 
 export default function Scene() {
   const axesHelper = useRef<THREE.AxesHelper>(null);
@@ -23,80 +16,17 @@ export default function Scene() {
     );
   }, []);
 
- 
+  const divRef = useRef<HTMLDivElement>(null);
 
   return (
-    <Canvas id="scene" style={{ height: "100vh" }} camera={{
-      position: [0.2, 1.8, -2],
-    }}>
+    <Canvas id="scene" style={{ height: "100vh" }}>
       <Suspense fallback={null}>
-        {/* <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} /> */}
-        {/* <MyModel position={[0, -2, 0]} /> */}
         <GalleryModel />
         <gridHelper args={[100, 100]} />
         <CustomControls />
         <axesHelper ref={axesHelper} args={[100]} />
-        {/* XZY */}
-        {/* <OrbitControls target={[20, 0,-10]} makeDefault  /> */}
         <Environment preset="sunset" background />
       </Suspense>
     </Canvas>
   );
-}
-
-function CustomControls() {
-  const controls = useRef<any>();
-  const isLocked = useRef(false);
-
-  const { camera } = useThree();
-
-  useEffect(()=>{
-    camera.position.set(0.5 , 1.8, -2)
-    camera.lookAt(1, 1.8, -2);
-  }, [])
-
-  const onKeyDown = function (event: KeyboardEvent) {
-    switch (event.code) {
-      case "KeyW":
-        controls.current.moveForward(0.11);
-        break;
-      case "KeyA":
-        controls.current.moveRight(-0.11);
-        break;
-      case "KeyS":
-        controls.current.moveForward(-0.11);
-        break;
-      case "KeyD":
-        controls.current.moveRight(0.11);
-        break;
-    }
-  };
-  document.addEventListener("keydown", onKeyDown, false);
-
-  return (
-    <PointerLockControls
-    position={new Vector3(100, 100, 100)}
-      onUpdate={() => {
-        if (controls.current) {
-          controls.current.addEventListener("lock", () => {
-            console.log("lock");
-            isLocked.current = true;
-          });
-          controls.current.addEventListener("unlock", () => {
-            console.log("unlock");
-            isLocked.current = false;
-          });
-        }
-      }}
-      ref={controls}
-    />
-  );
-}
-
-function CustomCamera(props: PerspectiveCameraProps) {
-  return <PerspectiveCamera 
-  position={[5, 5, 5]} near={0.1} far={10}
-  makeDefault
-  />;
 }
