9a7725f56613d0add4747df2aecc9130e28817df
Ivan Egorov <www.Len244@gmail.com>
2022-02-23 23:55:59 +0300

    Server done

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import { testRouter } from "./routes/testRoute";
+import { createClient } from "redis";
+import { apiRouter } from "./routes/apiRoute";
 //Initing .env file
 config();
 
@@ -14,9 +16,21 @@ app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT;
 
+export const redisClient = createClient();
+
+(async () => {
+  redisClient.on('error', (err) => console.log('Redis Client Error', err));
+  await redisClient.connect();
+
+  await redisClient.set('key', 'value');
+  const value = await redisClient.get('key');
+  console.log(value);
+  
+})();
 
 //Adding routes to app
 app.use("/testRoute", testRouter);
+app.use("/api/v1", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
---
+++ backend/routes/apiRoute.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { response, Router } from "express";
+import { redisClient } from "../app";
+
+interface pixabayResponse {
+  total: number;
+  totalHist: number;
+  hits: {
+    id: number;
+    pageURL: string;
+    largeImageURL: string;
+    fullHDURL: string;
+    imageURL: string;
+    imageWidth: number;
+    imageHeight: number;
+    imageSize: number;
+  }[]
+}
+
+const secondsInADay = 86400;
+
+export const apiRouter = Router();
+
+apiRouter.use("/:page", async (req, res)=>{
+  const pageToFetch = req.params.page;
+  const apiURL = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&page=${pageToFetch}&per_page=9`
+
+  if(!pageToFetch){
+    res.status(400).send("absent page param");
+  }
+
+  if (await redisClient.exists(pageToFetch)){
+    console.log("Redis hit. Retrieving data")
+    const pageData: pixabayResponse = JSON.parse(await redisClient.get(pageToFetch) ?? '');
+    res.send(pageData);
+  } else {
+    console.log("Didn't hit redis. Getting data from pixabay");
+    await axios.get(apiURL).then(data => {
+      res.send(data.data)
+      redisClient.set(pageToFetch, JSON.stringify(data.data));
+      redisClient.expire(pageToFetch, secondsInADay);
+    }).catch((error)=> res.status(500).send(`There was an error while calling api: ${error}`))
+    
+  }
+
+});
