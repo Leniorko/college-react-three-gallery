5adff0689f6b45862d1ee6f0106cff9f180298d3
Ivan Egorov <www.Len244@gmail.com>
2022-02-23 17:38:42 +0300

    Изменения в конфиг камеры

--- src/components/CameraControlsConfig.tsx
+++ src/components/CameraControlsConfig.tsx
@@ -11,39 +11,53 @@ import { Vector3 } from "three";
 // FIXME: Jittering movement. Should be smooth. Probably onKeyDown should add velocity and stop it onKeyUp
 export default function CustomControls() {
   const controls = useRef<any>(null);
+  const [isLocked, setIsLocked] = useState(false);
+  const [isFirstConfig, setIsFirtsConfig] = useState(true);
 
   const { camera } = useThree();
 
-  useEffect(()=>{
-    camera.position.set(0.5 , 1.8, -2)
-    camera.lookAt(1, 1.8, -2);    
-  }, [])
+  useEffect(() => {
+    camera.position.set(0.5, 1.8, -2);
+    camera.lookAt(1, 1.8, -2);
+  }, []);
 
-  useEffect(()=>{
-    const onKeyDown = function (event: KeyboardEvent) {
-      switch (event.code) {
-        case "KeyW":
-          controls.current.moveForward(0.11);
-          break;
-        case "KeyA":
-          controls.current.moveRight(-0.11);
-          break;
-        case "KeyS":
-          controls.current.moveForward(-0.11);
-          break;
-        case "KeyD":
-          controls.current.moveRight(0.11);
-          break;
-      }
-    };
-    if (controls.current){
-      document.addEventListener("keydown", onKeyDown, false);
+  const onKeyDown = function (event: KeyboardEvent) {
+    switch (event.code) {
+      case "KeyW":
+        controls.current.moveForward(0.11);
+        break;
+      case "KeyA":
+        controls.current.moveRight(-0.11);
+        break;
+      case "KeyS":
+        controls.current.moveForward(-0.11);
+        break;
+      case "KeyD":
+        controls.current.moveRight(0.11);
+        break;
     }
-    
-  }, [controls.current])
-  
- 
-  return (
-    <PointerLockControls ref={controls}/>
-  );
+  };
+
+  useEffect(() => {
+    if (controls.current && isLocked) {
+      document.addEventListener("keydown", onKeyDown, false);
+    } else {
+      console.log("Перед снятием");
+      
+      document.removeEventListener("keydown", onKeyDown, false);
+    }
+
+    if (controls.current && isFirstConfig) {
+      controls.current.addEventListener("lock", () => {
+        setIsLocked(true);
+      });
+      controls.current.addEventListener("unlock", () => {
+        setIsLocked(false);
+      });
+
+      setIsFirtsConfig(false);
+    }
+  }, [controls.current, isLocked]);
+
+  return <PointerLockControls ref={controls} />;
 }
